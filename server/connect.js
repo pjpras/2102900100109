5959944295d4a526d01ab45e8d4ed14a71d8fec9
Prashant Joshi <Bhairava@Prashants-MacBook-Air.local>
2024-07-05 20:54:26 +0530

    Frontend Change 1

--- server/connect.js
+++ server/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function ConnectToDb(URL){
-    const db = mongoose.connect(`mongodb://127.0.0.1:27017/${URL}`).then(console.log('Connected to MongoDB'));
+    const db = mongoose.connect(`mongodb://127.0.0.1:27017/${URL}`).then(console.log(`Connected to MongoDB`));
 }
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require('express')
+// const cors = require('cors');
 const {ConnectToDb} = require('./connect')
 const {urlRouter,redirectRoute} = require('./routes/url')
 
 const app = express();
-PORT = 8001;
+const port = process.env.PORT||8001;
 //connection
 try {
   ConnectToDb('short-url');
 } catch (error) {
   console.log(error)
 }
-
+// app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 //route
-app.use("/url",urlRouter);
+app.use("/api/url",urlRouter);
 app.use("/",redirectRoute);
 
-app.listen(PORT,()=>console.log(`Server started at port ${PORT}`))
+app.listen(port,()=>console.log(`Server started at port ${port}`))
